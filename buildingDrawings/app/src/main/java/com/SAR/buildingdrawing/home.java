02057a25aad9446e2978003b3d95ff7ef81b6424
HuzaifaSaeed021 <69757209+HuzaifaSaeed021@users.noreply.github.com>
2020-10-26 10:31:27 -0700

    build

---
+++ buildingDrawings/app/src/main/java/com/SAR/buildingdrawing/home.java
@@ -0,0 +1,115 @@
+package com.SAR.buildingdrawing;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.SAR.buildingdrawing.models.common;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        android.view.Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Building Drawings");
+
+//        if (common.currentUser != null){
+//            Toast.makeText(getApplicationContext(),""+ common.currentUser.getName()
+//                    +common.currentUser.getPhone()+common.currentUser.getType() +common.currentUser.getEmail(),Toast.LENGTH_SHORT).show();
+//        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                moveTaskToBack(true);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+
+            case R.id.home_nav:
+                //
+//                Intent intent2 = new Intent(home.this, home.class);
+//                startActivity(intent2);
+//                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                return true;
+
+            case R.id.build_house_nav:
+                //
+//                Intent intent1 = new Intent(home.this, .class);
+//                startActivity(intent1);
+//                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                return true;
+
+            case R.id.estimation_nav:
+                //
+                Intent intent3 = new Intent(home.this, estimation.class);
+                startActivity(intent3);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                return true;
+
+            case R.id._3d_buildings_nav:
+                //
+//                Intent intent2 = new Intent(home.this, .class);
+//                startActivity(intent2);
+//                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                return true;
+
+            case R.id.account_nav:
+                //
+                if (common.currentUser == null){
+                    Intent intent = new Intent(home.this, signin.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                }
+                else{
+                    Intent intent = new Intent(home.this, account.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                }
+                return true;
+
+
+        }
+        return false;
+    }
+}
